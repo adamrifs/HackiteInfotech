ed55faebd910e5453b43ad74def6be89c186b463
Adam <112921206+adamrifs@users.noreply.github.com>
2025-09-19 10:43:26 +0530

    edit blog

--- client/src/components/AddBlog.jsx
+++ client/src/components/AddBlog.jsx
@@ -7,8 +7,9 @@ import { toast, ToastContainer } from "react-toastify";
 
 const AddBlog = () => {
   const [blogModal, setBlogModal] = useState(false);
+  const [editBlog, setEditBlog] = useState(false)
+  const [blogsId, setBlogsId] = useState(null)
   const [blogs, setBlogs] = useState([])
-
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
   const [image, setImage] = useState(null)
@@ -59,13 +60,47 @@ const AddBlog = () => {
 
   //========================= delete a blog =====================================
 
-  const handleDeleteBlog = async (blogId) => {
+  const handleDeleteBlog = async (Id) => {
     try {
-      const response = await axios.delete(`http://localhost:5000/api/blog/deleteBlog/${blogId}`, { withCredentials: true })
+      const response = await axios.delete(`http://localhost:5000/api/blog/deleteBlog/${Id}`, { withCredentials: true })
       fetchBlogs()
     } catch (error) {
       console.log(error)
       alert(error)
+    }
+  }
+  //============================= edit a blog ====================================
+  const handleEditOpen = (blog) => {
+    setEditBlog(true)
+    setBlogsId(blog._id)
+    setTitle(blog.title)
+    setDescription(blog.description)
+    setImage(null)
+  }
+  const handleEditBlog = async (e) => {
+    try {
+      const formdata = new FormData()
+      if (image) formdata.append('image', image)
+      formdata.append('title', title)
+      formdata.append('description', description)
+
+      const response = await axios.put(`http://localhost:5000/api/blog/editBlog/${blogsId}`, formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        },
+        withCredentials: true
+      })
+      console.log("Updated:", response.data)
+      setTitle('')
+      setDescription('')
+      setBlogsId(null)
+      setEditBlog(false)
+      setImage(null)
+      fetchBlogs()
+      toast.success("Blog updated successfully")
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
     }
   }
   return (
@@ -93,7 +128,9 @@ const AddBlog = () => {
 
                 {/* Action Buttons */}
                 <div className="absolute top-2 right-2 flex gap-2">
-                  <button className="bg-gray-800 hover:bg-gray-700 !p-2 rounded-full cursor-pointer">
+                  <button
+                    onClick={() => handleEditOpen(item)}
+                    className="bg-gray-800 hover:bg-gray-700 !p-2 rounded-full cursor-pointer">
                     <FiEdit size={18} />
                   </button>
                   <button
@@ -164,6 +201,52 @@ const AddBlog = () => {
           </div>
         </div>
       )}
+
+      {/* ======================================= edit blog =================================  */}
+      {
+        editBlog && (
+          <div className="bg-black/80 fixed inset-0 flex items-center justify-center">
+            <div className="flex flex-col min-w-5xl bg-gray-800 rounded-2xl !p-5">
+              <h1 className="font-semibold text-2xl text-center !mb-8">edit a Blog</h1>
+              <label className="!mb-1">Image</label>
+              <input
+                type="file"
+                onChange={(e) => setImage(e.target.files[0])}
+                className="w-full !p-2 !mb-3 rounded-md border border-gray-700 focus:outline-none focus:ring-2"
+              />
+              <label className="!mb-1">Title</label>
+              <input
+                placeholder="Blog Title"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="w-full !p-2 !mb-3 rounded-md border border-gray-700 focus:outline-none focus:ring-2"
+              />
+              <label className="!mb-1">description</label>
+              <textarea
+                placeholder="Blog Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="w-full !p-2 !mb-3 rounded-md border border-gray-700 focus:outline-none focus:ring-2"
+              ></textarea>
+              <div className="flex justify-end gap-3">
+                <button
+                  onClick={() => setEditBlog(false)}
+                  className="bg-gray-600 hover:bg-gray-500 !px-4 !py-2 rounded"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleEditBlog}
+                  className="bg-blue-600 hover:bg-blue-500 !px-4 !py-2 rounded"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 };
