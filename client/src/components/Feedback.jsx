20012b2ef52edb582b059fc5185a3cb262b641d5
xhaadh <155063057+xhaadh@users.noreply.github.com>
2025-09-19 11:42:30 +0530

    profile

--- client/src/components/Feedback.jsx
+++ client/src/components/Feedback.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 // import avatar from "../assets/avatar.jpg";
-import { motion } from "framer-motion";
 
-const Feedback = ({image, name, role, content}) => {
+const Feedback = ({ name, role, content}) => {
   return (
     <div className="max-w-xs w-full group/card">
       <div
@@ -13,14 +12,14 @@ const Feedback = ({image, name, role, content}) => {
       >
         <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
 
-        <div className="flex flex-row items-center gap-3 z-10 !py-3 !px-4 ">
-          <img
+        <div className="flex flex-row items-center gap-3 z-10 !py-3 !px-5 ">
+          {/* <img
             height="100"
             width="100"
             alt="Avatar"
             src={image}
             className="h-10 w-10 rounded-full border-2 object-cover"
-          />
+          /> */}
           <div className="flex flex-col">
             <p className="font-normal text-left text-base !text-gray-50 relative z-10">
               {name}
